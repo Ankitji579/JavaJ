3b5fee831eb3768d8a1a642e840384475e3349bb
Ankit Vashisth <dryisholeup@gmail.com>
2023-06-02 21:09:10 +0530

    starting this

--- First.java
+++ First.java
@@ -1,6 +1,6 @@
-public class Main {
+public class First {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println("Hello duniya");
 
     }
 }
