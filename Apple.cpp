b247777b1ff511b85bc15da36473b022fddfb8f1
ANKIT VASHISTH <dryisholeup@gmail.com>
2023-06-02 14:58:00 +0530

    this is not final

---
+++ Apple.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //running in the following character is required and 
+    char f[23];
+    cin>> f; 
+    cout<<f;
+    return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char[23] a;
+    cin>> a;
+    cout<<a;
+    return 0;
+}
