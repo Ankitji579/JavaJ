1973a9d390922fcd0b43fdbf40fb173adf01c0c7
Ankit Vashisth <dryisholeup@gmail.com>
2023-06-03 09:47:14 +0530

    2nd attempt

--- First.java
+++ First.java
@@ -1,6 +1,6 @@
 public class First {
     public static void main(String[] args){
-        System.out.println("Hello duniya");
-        // this is just for sake
+        System.out.println("Very excited to learn Java");
+        
     }
 }
