5128e17a235a5f12ed687c013b228625b56e639e
Ankit Vashisth <dryisholeup@gmail.com>
2023-06-15 19:09:55 +0530

    This

--- Noob.java
+++ Noob.java
@@ -4,10 +4,15 @@ public class Noob {
         System.out.println("Write any number :");
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
+        int sum = 1;
+        if ( a == sum ){
+            System.out.println("Not valid");
+        }
+        else{
         System.out.println(a);
         System.out.println(a+1);
         System.out.println(a+2);
         System.out.println(a+3);
-        System.out.println(a+4);
+        System.out.println(a+4);}
     }
 }
