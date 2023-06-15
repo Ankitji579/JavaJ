e83b89690a41c506e5329ffbc62da911f11156b5
Ankit Vashisth <dryisholeup@gmail.com>
2023-06-15 16:35:53 +0530

    three

---
+++ Noob.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Noob {
+    public static void main(String[] args){
+        System.out.println("Write any number :");
+        Scanner sc = new Scanner(System.in);
+        boolean a = sc.nextBoolean();
+        System.out.println(a);
+    }
+}
