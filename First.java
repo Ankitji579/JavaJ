ddc044b97ed22c47edb443257105421b784a3d0b
Ankit Vashisth <dryisholeup@gmail.com>
2023-06-15 15:46:26 +0530

    one one one

--- First.java
+++ First.java
@@ -1,10 +1,17 @@
+import java.util.*;
 public class First {
     public static void main(String[] args){
-        System.out.println("Write any number ");
+        System.out.println("Write any number :");
         Scanner sc = new Scanner(System.in);
-        String num1 = sc.next();
-        System.out.println(num1);
-
+        // this function is for inputting strings only: Int num1 = sc.nextLine();
+        int num1 = sc.nextInt();
+        System.out.println("This is the number : " + num1);
+        if (num1 % 2 == 0){
+            System.out.println("The number is even");
+        }
+        else {
+            System.out.println("The number is odd");
+        }
          
     }
 }
