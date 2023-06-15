30e6449d3b0b79cfdb87900a6b81c2f3110d9840
Ankit Vashisth <dryisholeup@gmail.com>
2023-06-15 15:28:16 +0530

    this one is new

--- First.java
+++ First.java
@@ -2,7 +2,8 @@ public class First {
     public static void main(String[] args){
         System.out.println("Write any number ");
         Scanner sc = new Scanner(System.in);
-        Int num1 = sc.next();
+        String num1 = sc.next();
+        System.out.println(num1);
 
          
     }
