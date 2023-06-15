4c0bcdf942f4b8791c3d3ca595ed7dc51357addc
Ankit Vashisth <dryisholeup@gmail.com>
2023-06-15 16:15:21 +0530

    new one

--- First.java
+++ First.java
@@ -4,13 +4,15 @@ public class First {
         System.out.println("Write any number :");
         Scanner sc = new Scanner(System.in);
         // this function is for inputting strings only: Int num1 = sc.nextLine();
-        int num1 = sc.nextInt();
+        float num1 = sc.nextFloat();
         System.out.println("This is the number : " + num1);
-        if (num1 % 2 == 0){
-            System.out.println("The number is even");
+        float  sum  = num1 % 3;
+        
+        if (sum == 0){
+            System.out.println("The number is divisible by 3");
         }
         else {
-            System.out.println("The number is odd");
+            System.out.println("The number is not divisible by 3");
         }
          
     }
