a677e84fa37b6282e64b34ea983440a663e7718c
Ankit Vashisth <dryisholeup@gmail.com>
2023-06-15 15:23:57 +0530

    this is first file

--- First.java
+++ First.java
@@ -1,6 +1,9 @@
 public class First {
     public static void main(String[] args){
-        System.out.println("Very excited to learn Java");
+        System.out.println("Write any number ");
+        Scanner sc = new Scanner(System.in);
+        Int num1 = sc.next();
+
          
     }
 }
